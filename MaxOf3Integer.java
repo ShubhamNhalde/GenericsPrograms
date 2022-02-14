14533804646dee6a54354b94c7c7bb90a373e188
Shubham <nhaldes@gmail.com>
2022-02-14 21:50:21 +0530

    Added: Max Of 3 Integers

---
+++ MaxOf3Integer.java
@@ -0,0 +1,28 @@
+package com.bridgelabz.genericsproblem;
+
+import java.util.Scanner;
+
+public class MaxOf3Integer {
+	   public static void main(String[] args) {
+
+	        Scanner scanner = new Scanner(System.in);
+	        System.out.println("Enter three numbers: ");
+	        Integer x = scanner.nextInt();
+	        Integer y = scanner.nextInt();
+	        Integer z = scanner.nextInt();
+	        Integer max = -2147483648;
+	        int result = x.compareTo(max);
+	        if (result == 1) {
+	            max = x;
+	        }
+	        result = y.compareTo(max);
+	        if (result == 1) {
+	            max = y;
+	        }
+	        result = z.compareTo(max);
+	        if (result == 1) {
+	            max = z;
+	        }
+	        System.out.println("The max number is : " + max);
+	    }
+}
